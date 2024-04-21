acdf058647c35044b4873e516e7129d120d7532f
Alepac <alessandro.pacifici@gmail.com>
2024-04-21 09:34:01 +0200

    fixing service stop and adding better file rotation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-sc create AAASimpleHttpService binpath= C:\Users\Leonardo\Documents\Projects\SimpleHttpServer\dist\SimpleHttpServer.exe
-sc delete AAASimpleHttpService
+sc create SimpleHttpServer binpath= C:\Users\Leonardo\Documents\Projects\SimpleHttpServer\dist\SimpleHttpServer.exe
+sc delete SimpleHttpServer
+
+# Build:
+pyinstaller.exe --onefile --hidden-import win32timezone SimpleHttpServer.py
+
+# With Administrator privilges
+# Install:
+dist\SimpleHttpServer.exe install
+
+# Start:
+dist\SimpleHttpServer.exe start
+
+# Install with autostart:
+dist\SimpleHttpServer.exe --startup delayed install
+
+# Debug:
+dist\SimpleHttpServer.exe debug
+
+# Stop:
+dist\SimpleHttpServer.exe stop
+
+# Uninstall:
+dist\SimpleHttpServer.exe remove
--- SimpleHttpServer.py
+++ SimpleHttpServer.py
@@ -1,6 +1,7 @@
 import http.server
 import configparser
 import threading
+from threading import Event
 import logging
 import os
 import win32serviceutil
@@ -11,6 +12,9 @@ import socket
 import time
 import sys
 from datetime import datetime
+
+VERSION="0.0.1"
+
 default_values = {
     "httpPort": 8000
 }
@@ -20,29 +24,7 @@ web_folder = current_dir + '\public'
 log_folder = current_dir + '\log'
 serviceLogger = logging.getLogger(__name__)
 
-def create_daily_log_handler():
-    today = datetime.today()
-    if not os.path.exists(log_folder):
-        os.mkdir(log_folder)
-    log_filename = today.strftime(log_folder +'\http_server_%Y-%m-%d.log')
-    fileHandler = logging.FileHandler(log_filename)
-    fileHandler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)-7.7s %(message)s'))
-    for handler in serviceLogger.handlers[:]:
-        if isinstance(handler, logging.FileHandler):
-            serviceLogger.removeHandler(handler)
-            handler.close()
-    serviceLogger.addHandler(fileHandler)
-
 serviceLogger.setLevel(logging.DEBUG)
-
-
-def schedule_daily_log_handler():
-    while True:
-        create_daily_log_handler()
-        time.sleep(60*60*24)
-
-daily_log_handler_thread = threading.Thread(target=schedule_daily_log_handler)
-daily_log_handler_thread.start()
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
     # Override the __init__ method to specify the directory
@@ -63,14 +45,16 @@ class SimpleHttpService(win32serviceutil.ServiceFramework):
         self.log = serviceLogger
 
         socket.setdefaulttimeout(60)
-        self.isAlive = True
+        self.stop_event = Event()
+        self.start_event = Event()
+
+
 
     def SvcStop(self):
-        self.log.info("Stopping service")
+        self.log.info(f"Stopping service v{VERSION}")
+        self.stop_event.set()
         try:
-            self.isAlive = False
             self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-            self.log.info(f'Server stopping')
             self.server.shutdown()
             win32event.SetEvent(self.hWaitStop)
             self.log.info(f'Server stopped')
@@ -78,12 +62,41 @@ class SimpleHttpService(win32serviceutil.ServiceFramework):
             self.log.exception(e)
 
     def SvcDoRun(self):
-        self.isAlive = True
-        self.log.info("Starting service")
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE, 
                               servicemanager.PYS_SERVICE_STARTED, (self._svc_name_, ''))
+
+        daily_log_handler_thread = threading.Thread(target=self.Schedule_daily_log_handler)
+        daily_log_handler_thread.start()
+        self.start_event.wait()
+        self.log.info(f"Starting service v{VERSION}")
         self.Run()
         win32event.WaitForSingleObject(self.hWaitStop, win32event.INFINITE)
+    
+    def Create_daily_log_handler(self):
+        today = datetime.today()
+        if not os.path.exists(log_folder):
+            os.mkdir(log_folder)
+        log_filename = today.strftime(log_folder +'\http_server_%Y-%m-%d.log')
+        fileHandler = logging.FileHandler(log_filename)
+        fileHandler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)-7.7s %(message)s'))
+        for handler in self.log.handlers[:]:
+            if isinstance(handler, logging.FileHandler):
+                self.log.removeHandler(handler)
+                handler.close()
+        self.log.addHandler(fileHandler)
+
+    def Schedule_daily_log_handler(self):
+        first_run=True
+        while not self.stop_event.is_set():
+            self.Create_daily_log_handler()
+            if first_run:
+                self.start_event.set()
+                first_run=False
+            current_time = datetime.now()
+            midnight = datetime(year=current_time.year, month=current_time.month, day=current_time.day, hour=23, minute=59, second=59)
+            time_to_midnight = midnight - current_time
+            seconds_until_midnight = time_to_midnight.total_seconds() + 2
+            self.stop_event.wait(timeout=seconds_until_midnight)
 
     def Run(self):
         config = configparser.ConfigParser(default_values)
@@ -91,20 +104,23 @@ class SimpleHttpService(win32serviceutil.ServiceFramework):
 
         httpPort = int(config['DEFAULT']['httpPort'])
 
-        while self.isAlive:
+        while not self.stop_event.is_set():
             try:
                 self.server = http.server.ThreadingHTTPServer(('localhost', httpPort), MyRequestHandler)
                 self.log.info(f'Serving on port {httpPort} from "{web_folder}"')
                 self.server.serve_forever()
             except Exception as e:
                 self.log.info(f"Error starting HTTP server: {e}")
-                self.isAlive = False
+                self.stop_event.set()
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        servicemanager.Initialize()
-        servicemanager.PrepareToHostSingle(SimpleHttpService)
-        servicemanager.StartServiceCtrlDispatcher()
+        try:
+            servicemanager.Initialize()
+            servicemanager.PrepareToHostSingle(SimpleHttpService)
+            servicemanager.StartServiceCtrlDispatcher()
+        except Exception:
+            win32serviceutil.HandleCommandLine(SimpleHttpService)
     else:
         win32serviceutil.HandleCommandLine(SimpleHttpService)
 
